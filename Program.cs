9ea512a6674c996b33da6f1bf333622e2a1b0ff9
Eberhard Beilharz <eb1@sil.org>
2015-09-07 23:16:53 +0200

    Add test app

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) 2015 Eberhard Beilharz
+// This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
+using System;
+using SIL.Utils;
+using System.Windows.Forms;
+
+namespace TestingErrorReport
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			//ErrorReporter.ReportException(new ApplicationException("Test"), null, "someone@example.com");
+			//ErrorReporter.ReportException(new ApplicationException("Test"), null, "someone@example.com", null, false);
+			//ErrorReporter.ReportProblem(null, "someone@example.com", null);
+			ErrorReporter.ReportDuplicateGuids(null, "someone@example.com", null, "My error text");
+			ErrorReporter.MakeSuggestion(null, "someone@example.com", null);
+			Application.Run(new Form());
+		}
+	}
+}
